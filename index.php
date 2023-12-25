08673ffc22dc6ec118c58868b333f3c7d58a15aa
hodge-py <105604814+hodge-py@users.noreply.github.com>
2023-12-25 08:03:28 +0000

    style changes

--- index.php
+++ index.php
@@ -258,20 +258,6 @@
 </td>
                 </tr>
 
-              <tr>
-              <td scope="row"></td>
-                  <td>
-                    <a type="button" class="btn btn-primary" href="https://www.dropbox.com/scl/fi/3e2jb0s1niz03wc429k1v/peerair-win32-x64.zip?rlkey=tr0hxlsz2a6rec37iduhoi9s0&dl=1" download>
-                  <svg class="me-2" width="24px" height="24px" viewBox="0 0 20 20" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" fill="#ffffff"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <title>windows [#174]</title> <desc>Created with Sketch.</desc> <defs> </defs> <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"> <g id="Dribbble-Light-Preview" transform="translate(-60.000000, -7439.000000)" fill="#ffffff"> <g id="icons" transform="translate(56.000000, 160.000000)"> <path d="M13.1458647,7289.43426 C13.1508772,7291.43316 13.1568922,7294.82929 13.1619048,7297.46884 C16.7759398,7297.95757 20.3899749,7298.4613 23.997995,7299 C23.997995,7295.84873 24.002005,7292.71146 23.997995,7289.71311 C20.3809524,7289.71311 16.7649123,7289.43426 13.1458647,7289.43426 M4,7289.43526 L4,7296.22153 C6.72581454,7296.58933 9.45162907,7296.94113 12.1724311,7297.34291 C12.1774436,7294.71736 12.1704261,7292.0908 12.1704261,7289.46524 C9.44661654,7289.47024 6.72380952,7289.42627 4,7289.43526 M4,7281.84344 L4,7288.61071 C6.72581454,7288.61771 9.45162907,7288.57673 12.1774436,7288.57973 C12.1754386,7285.96017 12.1754386,7283.34361 12.1724311,7280.72405 C9.44461153,7281.06486 6.71679198,7281.42567 4,7281.84344 M24,7288.47179 C20.3879699,7288.48578 16.7759398,7288.54075 13.1619048,7288.55175 C13.1598997,7285.88921 13.1598997,7283.22967 13.1619048,7280.56914 C16.7689223,7280.01844 20.3839599,7279.50072 23.997995,7279 C24,7282.15826 23.997995,7285.31353 24,7288.47179" id="windows-[#174]"> </path> </g> </g> </g> </g></svg>
-                  Download for Windows</a>
-                  </td>
-                  <td style='height:10px;'>
-                    <a type="button" class="btn btn-primary my-auto" href="#" style='width: 14em; height: 100%;'>
-                      Subscribe
-                  </a>
-                  </td>
-                </tr>
-
               </tbody>
             </table>
           </div>
@@ -284,8 +270,29 @@
         </div>
 
 
+   <div class="container-fluid" style=" background-color: rgba(0, 0, 0, 0.384); color: rgba(255, 255, 255,1); padding: 2.5em 9.375em 0 9.375em;">
 
+  <div class='d-flex justify-content-evenly'>
+
+    <div class='card bg-black'>
+      <div class='card-header'>
+      <h1>Free</h1>
+      </div>
+      <div class='card-body'>
+        <div>test</div>
         
+      </div>
+    </div>
+
+    <div>
+      <h1>Pricing</h1>
+    </div>
+
+  </div>
+
+
+   </div>
+
 
 
         <div class="text-end" style="background-color: rgba(20, 20, 20, 0.473); color:white;">
