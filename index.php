44bcbf338d7d4dcb94d3076d80311946fffc6845
hodge-py <105604814+hodge-py@users.noreply.github.com>
2023-12-26 08:38:19 +0000

    pricing

--- index.php
+++ index.php
@@ -22,7 +22,7 @@
 </head>
 <body style="width: 100%;" class="" >
 
-    <div class="row p-3 border-end-0 shadow" style="backdrop-filter: blur(10px); position: fixed; width: 100vw; margin: 0px;">
+    <div class="row p-3 border-end-0 shadow" style="backdrop-filter: blur(10px); position: fixed; width: 100vw; margin: 0px; z-index: 99;">
     <div class="col-md-10">
     <h1 class="text-start ps-5 text-white">&#9736; PeerAir</h1>
     </div>
@@ -41,7 +41,7 @@
     <div class="" style="padding-bottom: 85px;"></div>
     
     <div class="container-fluid" style=" background-color: rgba(0, 0, 0, 0.384); color: rgba(255, 255, 255,1); width: 100%; padding: 2.5em 9.375em 0 9.375em;">
-    <div class="row pb-5" style="margin: 0px; padding-top: 30px;">
+    <div class="row pb-3" style="margin: 0px; padding-top: 30px;">
         <div class="col-md-12 pb-3 text-center" style="text-align: justify;">
     
         <div class="" style="">
@@ -79,7 +79,7 @@
     
     
     <div class="container ">
-    <div class="row border-3" style="margin: 0px;">
+    <div class="row" style="margin: 0px;">
         
 
     
@@ -87,10 +87,7 @@
     
     </div>
     
-    
-    
-    
-    
+        
     
     
     <div class="container-fluid" style=" background-color: rgba(0, 0, 0, 0.384); color: rgba(255, 255, 255,1); padding: 2.5em 9.375em 0 9.375em;">
@@ -164,8 +161,62 @@
         </div>
     
     
-    
-    
+     <div class="container-fluid text-center" style=" background-color: rgba(0, 0, 0, 0.384); color: rgba(255, 255, 255,1); padding: 2.5em 9.375em 0 9.375em;">
+
+       <h1 class='pb-3'>Pricing</h1>
+    <div class='d-flex justify-content-center'>
+
+      <div class="card text-bg-dark text-center mb-3 w-100" style=''>
+        <div class='d-flex card-header flex-column justify-content-center'>
+          <h2 class=''>Free</h2>
+          <div class='btn' style='visibility: hidden;'>hide</div>
+        </div>
+        <div class="card-body">
+          <h5 class="card-title"></h5>
+          <div class="card-text">
+            <a type="button" class="btn btn-primary p-3" href="https://www.dropbox.com/scl/fi/3e2jb0s1niz03wc429k1v/peerair-win32-x64.zip?rlkey=tr0hxlsz2a6rec37iduhoi9s0&dl=1" download>
+          <svg class="me-2" width="32px" height="32px" viewBox="0 0 20 20" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" fill="#ffffff"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <title>windows [#174]</title> <desc>Created with Sketch.</desc> <defs> </defs> <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"> <g id="Dribbble-Light-Preview" transform="translate(-60.000000, -7439.000000)" fill="#ffffff"> <g id="icons" transform="translate(56.000000, 160.000000)"> <path d="M13.1458647,7289.43426 C13.1508772,7291.43316 13.1568922,7294.82929 13.1619048,7297.46884 C16.7759398,7297.95757 20.3899749,7298.4613 23.997995,7299 C23.997995,7295.84873 24.002005,7292.71146 23.997995,7289.71311 C20.3809524,7289.71311 16.7649123,7289.43426 13.1458647,7289.43426 M4,7289.43526 L4,7296.22153 C6.72581454,7296.58933 9.45162907,7296.94113 12.1724311,7297.34291 C12.1774436,7294.71736 12.1704261,7292.0908 12.1704261,7289.46524 C9.44661654,7289.47024 6.72380952,7289.42627 4,7289.43526 M4,7281.84344 L4,7288.61071 C6.72581454,7288.61771 9.45162907,7288.57673 12.1774436,7288.57973 C12.1754386,7285.96017 12.1754386,7283.34361 12.1724311,7280.72405 C9.44461153,7281.06486 6.71679198,7281.42567 4,7281.84344 M24,7288.47179 C20.3879699,7288.48578 16.7759398,7288.54075 13.1619048,7288.55175 C13.1598997,7285.88921 13.1598997,7283.22967 13.1619048,7280.56914 C16.7689223,7280.01844 20.3839599,7279.50072 23.997995,7279 C24,7282.15826 23.997995,7285.31353 24,7288.47179" id="windows-[#174]"> </path> </g> </g> </g> </g></svg>
+          Download for Windows</a>          </div>
+        </div>
+      </div>
+
+
+      <div class="card text-bg-dark mb-3 text-center w-100" >
+        <div class="card-header d-flex flex-column">
+          <h2 class=''>Premium</h2>
+          <div class='d-flex justify-content-center'>
+          <input type="radio" class="btn-check" name="options" id="option1" autocomplete="off" checked>
+          <label class="btn btn-outline-secondary flex-grow-1" for="option1">Monthly</label>
+
+          <input type="radio" class="btn-check" name="options" id="option2" autocomplete="off">
+          <label class="btn btn-outline-secondary flex-grow-1" for="option2">Yearly</label>
+        </div>
+        </div>
+          
+        <div class="card-body">
+
+
+          <div class="card-text">
+            <div id='month-pay' class='display-6 fw-bold pb-3'>$2.99/Mo</div>
+            <div id='year-pay' class='display-6 fw-bold pb-3' style='display:none;'>$29.99/Yr</div>
+
+            <a href='https://buy.stripe.com/eVafZ0feh3E5ciQ3cc' class='btn btn-primary' id='month-btn' style='display:block;' >Subscribe Mo.</a>
+            <a href='https://buy.stripe.com/3cs0025DH5Md96E145' class='btn btn-primary' id='year-btn' style='display:none;'>Subscribe Yr.</a>
+
+          </div>
+
+        </div>
+      </div>
+    </div>
+
+    </div>
+
+
+
+
+
+
+  
     
     <div class="container-fluid" style=" background-color: rgba(0, 0, 0, 0.384); color: rgba(255, 255, 255,1); padding: 2.5em 9.375em 0 9.375em;">
         <div class="" style=" margin: 0px;">
@@ -270,46 +321,7 @@
         </div>
 
 
-   <div class="container-fluid" style=" background-color: rgba(0, 0, 0, 0.384); color: rgba(255, 255, 255,1); padding: 2.5em 9.375em 0 9.375em;">
 
-  <div class='d-flex justify-content-evenly'>
-
-    <div class="card text-bg-dark text-center mb-3">
-      <div class="card-header">Free</div>
-      <div class="card-body">
-        <h5 class="card-title"></h5>
-        <div class="card-text">
-          <a type="button" class="btn btn-primary p-3" href="https://www.dropbox.com/scl/fi/3e2jb0s1niz03wc429k1v/peerair-win32-x64.zip?rlkey=tr0hxlsz2a6rec37iduhoi9s0&dl=1" download>
-        <svg class="me-2" width="32px" height="32px" viewBox="0 0 20 20" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" fill="#ffffff"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <title>windows [#174]</title> <desc>Created with Sketch.</desc> <defs> </defs> <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"> <g id="Dribbble-Light-Preview" transform="translate(-60.000000, -7439.000000)" fill="#ffffff"> <g id="icons" transform="translate(56.000000, 160.000000)"> <path d="M13.1458647,7289.43426 C13.1508772,7291.43316 13.1568922,7294.82929 13.1619048,7297.46884 C16.7759398,7297.95757 20.3899749,7298.4613 23.997995,7299 C23.997995,7295.84873 24.002005,7292.71146 23.997995,7289.71311 C20.3809524,7289.71311 16.7649123,7289.43426 13.1458647,7289.43426 M4,7289.43526 L4,7296.22153 C6.72581454,7296.58933 9.45162907,7296.94113 12.1724311,7297.34291 C12.1774436,7294.71736 12.1704261,7292.0908 12.1704261,7289.46524 C9.44661654,7289.47024 6.72380952,7289.42627 4,7289.43526 M4,7281.84344 L4,7288.61071 C6.72581454,7288.61771 9.45162907,7288.57673 12.1774436,7288.57973 C12.1754386,7285.96017 12.1754386,7283.34361 12.1724311,7280.72405 C9.44461153,7281.06486 6.71679198,7281.42567 4,7281.84344 M24,7288.47179 C20.3879699,7288.48578 16.7759398,7288.54075 13.1619048,7288.55175 C13.1598997,7285.88921 13.1598997,7283.22967 13.1619048,7280.56914 C16.7689223,7280.01844 20.3839599,7279.50072 23.997995,7279 C24,7282.15826 23.997995,7285.31353 24,7288.47179" id="windows-[#174]"> </path> </g> </g> </g> </g></svg>
-        Download for Windows</a>          </div>
-      </div>
-    </div>
-    
-
-    <div class="card text-bg-dark mb-3 text-center" >
-      <div class="card-header">
-        <div>Premium</div>
-        <input type="radio" class="btn-check" name="options" id="option1" autocomplete="off" checked>
-        <label class="btn btn-outline-secondary" for="option1">Monthly</label>
-
-        <input type="radio" class="btn-check" name="options" id="option2" autocomplete="off">
-        <label class="btn btn-outline-secondary" for="option2">Yearly</label>
-      </div>
-      <div class="card-body">
-        
-
-        <div class="card-text">
-          <div class='display-6'>$2.99/Mo</div>
-
-        </div>
-        
-      </div>
-    </div>
-    
-  </div>
-
-
-   </div>
 
 
 
@@ -387,7 +399,26 @@ a:active {
 
     });
 
+
+    $(document).on('click','#option1', function () {
+        $('#month-pay').css('display','block');
+        $('#year-pay').css('display','none');
+
+      $('#month-btn').css('display','block');
+      $('#year-btn').css('display','none');
+
     });
+
+      $(document).on('click','#option2', function () {
+          $('#month-pay').css('display','none');
+          $('#year-pay').css('display','block');
+
+        $('#month-btn').css('display','none');
+        $('#year-btn').css('display','block');
+
+      });
+
+      });
     
     </script>
     
